4cc0b9d9c6b364c87471a4c331db643a9ee72ac0
Borja <Borja@DESKTOP-43RC6QL>
2025-02-25 19:38:51 +0100

    Refactorización: Renombrar miCuenta a cuenta1 y crear operativa_cuenta

--- src/main/java/com/mycompany/ed04tarea/cuentas/Main.java
+++ src/main/java/com/mycompany/ed04tarea/cuentas/Main.java
@@ -3,23 +3,28 @@ package com.mycompany.ed04tarea.cuentas;
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        operativa_cuenta(695);  // Llamamos al nuevo método con una cantidad de prueba
+    }
+
+    public static void operativa_cuenta(float cantidad) {
+        CCuenta cuenta1;
         double saldoActual;
 
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es " + saldoActual);
 
         try {
-            miCuenta.retirar(2300);
+            cuenta1.retirar(2300);
         } catch (Exception e) {
-            System.out.print("Fallo al retirar");
+            System.out.println("Fallo al retirar");
         }
+
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            cuenta1.ingresar(cantidad);
         } catch (Exception e) {
-            System.out.print("Fallo al ingresar");
+            System.out.println("Fallo al ingresar");
         }
     }
 }
